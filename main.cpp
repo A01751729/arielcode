47e1fba0bc7a0d4e69bcf46ebfa87b32aa532998
A01751433 <149409873+A01751433@users.noreply.github.com>
2024-09-09 12:33:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,40 @@
 
 int main()
 {
+    LinkedList<int> a;
+    LinkedList<std::string> b;
+    LinkedList<double> c;
+
+    std::cout << a.to_string() << "\n";
+
+    a.insert_front(4);
+    a.insert_front(6);
+    a.insert_front(10);
+
+    std::cout << a.to_string() << "\n";
+
+    b.insert_front("One");
+    b.insert_front("Two");
+    b.insert_front("Three");
+    b.insert_front("Four");
+
+    std::cout << b.to_string() << "\n";
+
+    c.insert_front(3.1416);
+    c.insert_front(2.718);
+    c.insert_front(1.412);
+
+    std::cout << c.to_string() << "\n";
+
+    a.insert_back(4);
+    a.insert_back(10);
+    a.insert_back(6);
+    std::cout << "a.size() = " << a.size() << "\n";
+    std::cout << a.to_string() << "\n";
+
+    std::cout << b.remove_front() << "\n";
+    std::cout << "b.size() = " << b.size() << "\n";
+    std::cout << b.to_string() << "\n";
+
     return 0;
 }
